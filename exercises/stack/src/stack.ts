6e845608ebf74c2180ca9dcd6b6ea2e3554f8cf8
Maosen <maosen1993@gmail.com>
2017-12-10 11:55:19 -0500

    exercise 5

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,46 @@ interface IStack<T> {
   print(): void;
 }
 
-export class Stack<T> {
+interface IStackNode<T> {
+  data: T;
+  next: IStackNode<T>;
+}
 
+export class Stack<T> implements IStack<T> {
+  private head: IStackNode<T>;
+
+  push(item: T): Stack<T>;
+  push(items: T[]): Stack<T>;
+
+  push(itemOrArray: T | T[]): Stack<T> {
+    if (itemOrArray instanceof Array) {
+      itemOrArray.forEach(item => this.push(item));
+      return this;
+    }
+    let newNode = { data: itemOrArray, next: this.head };
+    this.head = newNode;
+    return this;
+  }
+  pop(): T | undefined {
+    let n = this.head;
+    if (!n) return undefined;
+    this.head = this.head.next;
+    return n.data;
+  }
+  length(): number {
+    let n = this.head;
+    let l = 0;
+    while (n) {
+      l++;
+      n = n.next;
+    }
+    return l;
+  }
+  print(): void {
+    let n = this.head;
+    while (n) {
+      console.log(n.data);
+      n = n.next;
+    }
+  }
 }
